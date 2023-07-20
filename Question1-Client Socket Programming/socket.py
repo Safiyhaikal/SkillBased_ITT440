491b783adb8c588ef24b6b6c4b2a75fd6b0abd93
Safiyhaikal <129300567+Safiyhaikal@users.noreply.github.com>
2023-07-20 10:19:54 +0800

    Create socket.py

---
+++ Question1-Client Socket Programming/socket.py
@@ -0,0 +1,33 @@
+import socket
+
+def connect_to_server():
+    # Server information
+    server_host = "izani.synology.me"
+    server_port = 8443
+
+    # UiTM Student ID
+    student_id = "2021887274"
+
+    # Create a socket object
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Connect to the server
+        client_socket.connect((server_host, server_port))
+        print("Connected to the server!")
+
+        # Send the Student ID to the server
+        client_socket.send(student_id.encode())
+
+        # Receive the server response
+        response = client_socket.recv(1024).decode()
+        print("Server response:", response)
+
+        # Close the connection
+        client_socket.close()
+
+    except Exception as e:
+        print("Error:", e)
+
+if __name__ == "__main__":
+    connect_to_server()
